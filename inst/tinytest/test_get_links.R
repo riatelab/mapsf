243acfd51bd53ce049b9f7012afcd6878c69b61d
rCarto <timothee.giraud@cnrs.fr>
2021-02-23 11:59:58 +0100

    test: add tests for warnings and errors in graduated symbols plots and links generation

--- inst/tinytest/test_get_links.R
+++ inst/tinytest/test_get_links.R
@@ -6,4 +6,5 @@ mob_97209 <- mob[mob$i == 97209, ]
 expect_silent({mob_links <- mf_get_links(x = mtq, df = mob_97209)})
 expect_equal(nrow(mob_links), 10)
 
-
+expect_error({mob_links <- mf_get_links(x = mtq, df = mob_97209, x_id = "LIBGEO")})
+expect_warning({mob_links <- mf_get_links(x = mtq[1:10,], df = mob_97209)})
--- inst/tinytest/test_map_g.R
+++ inst/tinytest/test_map_g.R
@@ -1,3 +1,7 @@
 mtq <- mf_get_mtq()
 mf_map(mtq)
 expect_silent(mf_map(st_cast(mtq, "MULTILINESTRING"), var = "POP", type = "grad"))
+expect_error(mf_map(st_cast(mtq, "MULTILINESTRING"), var = "POP", type = "grad",
+                    lwd = c(1,2)))
+expect_error(mf_map(mtq, var = "POP", type = "grad",
+                    cex = c(1,2)))
