ada07768473975469684b35b657155277d17f6f3
rCarto <timothee.giraud@cnrs.fr>
2021-03-10 16:50:38 +0100

    test: improve mf_theme() coverage

--- inst/tinytest/test_theme.R
+++ inst/tinytest/test_theme.R
@@ -14,4 +14,8 @@ expect_silent(mf_theme(x = list(
   cex = 2,
   font = 3
 )))
-expect_silent(mf_theme(bg = "green"))
+expect_silent(mf_theme(bg = "darkslategrey", fg = "cornsilk3",
+                       mar = c(2, 2, 4, 2),
+                       tab = FALSE, pos = "center", inner = FALSE,
+                       line = 2, cex = 2, font = 4)
+)
