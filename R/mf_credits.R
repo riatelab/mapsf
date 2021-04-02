209afe759748bc3aa9d7aa228a9b3316460055e7
rCarto <timothee.giraud@cnrs.fr>
2021-04-02 11:35:19 +0200

    feat: add an optional background to mf_credits()

--- R/mf_credits.R
+++ R/mf_credits.R
@@ -6,14 +6,19 @@
 #' @param txt text of the credits, use '\\n' to add line breaks
 #' @param cex cex of the credits
 #' @param font font of the credits
+#' @param bg background color
 #' @export
 #' @return No return value, credits are displayed.
 #' @examples
 #' mtq <- mf_get_mtq()
 #' mf_map(mtq)
 #' mf_credits(txt = "Author\nSources - Year")
-mf_credits <- function(txt = "Source(s) & Author(s)", pos = "bottomleft",
-                       col, cex = .6, font = 3) {
+mf_credits <- function(txt = "Source(s) & Author(s)",
+                       pos = "bottomleft",
+                       col,
+                       cex = .6,
+                       font = 3,
+                       bg = NA) {
   if (missing(col)) {
     col <- .gmapsf$args$fg
   }
@@ -21,30 +26,59 @@ mf_credits <- function(txt = "Source(s) & Author(s)", pos = "bottomleft",
   pw <- pd[2] - pd[1]
   pdp <- strwidth("M", units = "user", cex = 1) / 2
 
-  parapos <- switch(pos,
+
+  w <- strwidth(s = txt, units = "user", cex = cex, font = font)
+  h <- strheight(s = txt, units = "user", cex = cex, font = font)
+
+
+  ppos <- switch(pos,
     bottomleft = {
+      xr <- pd[1] + w + 2 * pdp
+      xl <- pd[1]
+      yb <- pd[3]
+      yt <- pd[3] + h + 2 * pdp
       pd[1] <- pd[1] + pdp
       pd[3] <- pd[3] + pdp
-      list(pd = pd, adj = c(0, 0), srt = 0)
+      list(
+        pd = pd, adj = c(0, 0), srt = 0,
+        xr = xr, xl = xl, yb = yb, yt = yt
+      )
     },
     bottomright = {
+      xr <- pd[2]
+      xl <- pd[2] - w - 2 * pdp
+      yb <- pd[3]
+      yt <- pd[3] + h + 2 * pdp
       pd[1] <- pd[2] - pdp
       pd[3] <- pd[3] + pdp
-      list(pd = pd, adj = c(1, 0), srt = 0)
+      list(
+        pd = pd, adj = c(1, 0), srt = 0,
+        xr = xr, xl = xl, yb = yb, yt = yt
+      )
     },
     rightbottom = {
+      xr <- pd[2]
+      xl <- pd[2] - h - 2 * pdp
+      yb <- pd[3]
+      yt <- pd[3] + w + 2 * pdp
       pd[1] <- pd[2] - pdp
       pd[3] <- pd[3] + pdp
-      list(pd = pd, adj = c(0, 0), srt = 90)
+      list(
+        pd = pd, adj = c(0, 0), srt = 90,
+        xr = xr, xl = xl, yb = yb, yt = yt
+      )
     }
   )
 
-  pd <- parapos$pd
-  adj <- parapos$adj
-  srt <- parapos$srt
+
+  rect(
+    xleft = ppos$xl, ybottom = ppos$yb, xright = ppos$xr,
+    ytop = ppos$yt, col = bg, border = NA, xpd = TRUE
+  )
+
   text(
-    x = pd[1], y = pd[3], labels = txt,
-    cex = cex, xpd = TRUE, adj = adj,
-    col = col, srt = srt, font = font
+    x = ppos$pd[1], y = ppos$pd[3], labels = txt,
+    cex = cex, xpd = TRUE, adj = ppos$adj,
+    col = col, srt = ppos$srt, font = font
   )
 }
