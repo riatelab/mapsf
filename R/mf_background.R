b8aa17984e03b3b55e721bc9ec9cecaa1e8e51c8
rCarto <timothee.giraud@cnrs.fr>
2021-06-15 13:30:26 +0200

    feat: add mf_background(), a function to plot a background image for a map

---
+++ R/mf_background.R
@@ -0,0 +1,45 @@
+#' @title Plot a background image
+#' @description Plot a background image on an existing plot
+#' @param filename filename of the background image, PNG or JPG/JPEG format.
+#' @param ... further parameters for
+#' \code{\link[graphics:rasterImage]{rasterImage}}
+#'
+#' @return No return value, a background image is displayed.
+#' @export
+#'
+#' @examples
+#' mtq <- mf_get_mtq()
+#' mf_init(mtq)
+#' mf_background(system.file("img/background.jpg", package = "mapsf"))
+#' mf_map(mtq, lwd = 3, col = NA, border = "white", add = TRUE)
+#' mf_credits(txt = 'Background photo by Noita Digital on Unsplash',
+#'            col = "white")
+mf_background <- function(filename, ...){
+  ex <- strsplit(basename(filename), split="\\.")[[1]]
+  ex <- tolower(ex[length(ex)])
+  if(ex == "png"){
+    if (!requireNamespace("png", quietly = TRUE)) {
+      stop(
+        "'png' package needed for this function to work. Please install it.",
+        call. = FALSE
+      )
+    }
+    img <- png::readPNG(filename)
+  }
+  if(ex %in% c("jpg", "jpeg")){
+    if (!requireNamespace("jpeg", quietly = TRUE)) {
+      stop(
+        "'jpeg' package needed for this function to work. Please install it.",
+        call. = FALSE
+      )
+    }
+    img <- jpeg::readJPEG(filename)
+  }
+  pusr <- par("usr")
+  graphics::rasterImage(image = img,
+              xleft = pusr[1],
+              ybottom = pusr[3],
+              xright =  pusr[2],
+              ytop = pusr[4],
+              ...)
+}
---
+++ inst/tinytest/test_background.R
@@ -0,0 +1,4 @@
+mtq <- mf_get_mtq()
+mf_init(mtq)
+expect_silent(mf_background(system.file("img/background.jpg", package = "mapsf")))
+expect_silent(mf_background(system.file("help/figures/logo.png", package = "mapsf")))
