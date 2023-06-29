562569895dbd88077ce4eec2dce2d4c799dc6b4b
rCarto <timothee.giraud@cnrs.fr>
2023-06-29 09:44:02 +0200

    test: update test for empty plot

--- inst/tinytest/test_pkg_utils.R
+++ inst/tinytest/test_pkg_utils.R
@@ -1,6 +1,9 @@
+if(!is.null(dev.list())){
+  dev.off()
+}
+expect_error(mapsf:::test_cur_plot())
 mtq <- mf_get_mtq()
 expect_silent(mapsf:::load_default_theme())
-expect_error(mapsf:::test_cur_plot())
 m <- sf::st_transform(mtq, "EPSG:4326")
 mf_map(m)
 expect_error(mf_scale())
