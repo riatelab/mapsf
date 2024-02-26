da42c2d87860eb8b45a597181360fe710bfb8d02
rCarto <timothee.giraud@cnrs.fr>
2024-02-26 16:33:20 +0100

    update doc

--- R/mf_map.R
+++ R/mf_map.R
@@ -8,10 +8,7 @@
 #' * `var`, the name(s) of a variable(s) to map;
 #' * `type`, the map layer type.
 #'
-#' Many parameters are available to fine tune the cartographic representations.
-#' These parameters are the common ones found in GIS and automatic cartography
-#' tools such as  classification and color palettes used in choropleth maps,
-#' or symbols sizes and shapes used in proportional symbols maps.
+#' Many parameters are available to fine tune symbologies and legends.
 #'
 #' Relevant arguments and default values are different for each map type and are
 #' described in the "Details" section.
@@ -30,12 +27,13 @@
 #'
 #' @details
 #' ## Relevant arguments and default values for each map types:
-#' * **base**: displays sf objects geometries.
+#' **base**: displays sf objects geometries.
 #' \preformatted{
 #' mf_map(x, col = "grey80", pch = 20, cex = 1, border = "grey20", lwd = 0.7,
 #'        expandBB, add = FALSE, ...)
 #'        }
-#' * **prop**: displays symbols with areas proportional to a quantitative
+#'
+#' **prop**: displays symbols with areas proportional to a quantitative
 #' variable (stocks). `inches` is used to set symbols sizes.
 #' \preformatted{
 #' mf_map(x, var, type = "prop", inches = 0.3, val_max, symbol = "circle",
@@ -48,7 +46,8 @@
 #'        leg_bg = getOption("mapsf.bg"), leg_fg = getOption("mapsf.fg"),
 #'        leg_size = 1)
 #'        }
-#' * **choro**: areas are shaded according to the variation of a quantitative
+#'
+#' **choro**: areas are shaded according to the variation of a quantitative
 #' variable. Choropleth maps are used to represent ratios or indices.
 #' `nbreaks`, and `breaks` allow to set the variable classification.
 #' Colors palettes, defined with `pal`, can be created with `mf_get_pal()` or
@@ -65,7 +64,8 @@
 #'        leg_fg = getOption("mapsf.fg"), leg_size = 1,
 #'        leg_box_border = getOption("mapsf.fg"), leg_box_cex = c(1, 1))
 #'        }
-#' * **typo**: displays a typology map of a qualitative variable.
+#'
+#' **typo**: displays a typology map of a qualitative variable.
 #' `val_order` is used to set modalities order in the legend.
 #' \preformatted{
 #' mf_map(x, var, type = "typo", pal = "Dynamic", alpha = 1, rev = FALSE,
@@ -79,7 +79,7 @@
 #'        leg_bg = getOption("mapsf.bg"), add = FALSE)
 #'        }
 #'
-#' * **symb**: displays the different modalities of a qualitative variable as
+#' **symb**: displays the different modalities of a qualitative variable as
 #' symbols.
 #' \preformatted{
 #' mf_map(x, var, type = "symb", pal = "Dynamic", alpha = 1, rev = FALSE,
@@ -92,7 +92,7 @@
 #'        leg_bg = getOption("mapsf.bg"), leg_size = 1, add = TRUE)
 #'        }
 #'
-#' * **grad**: displays graduated symbols. Sizes classes are set with
+#' **grad**: displays graduated symbols. Sizes classes are set with
 #' `breaks` and `nbreaks`. Symbol sizes are set with `cex`.
 #' \preformatted{
 #' mf_map(x, var, type = "grad", breaks = "quantile", nbreaks = 3, col = "tomato4",
@@ -104,7 +104,8 @@
 #'        leg_bg = getOption("mapsf.bg"), leg_frame_border = getOption("mapsf.fg"),
 #'        add = TRUE)
 #'        }
-#' * **prop_choro**: displays symbols with sizes proportional to values of a
+#'
+#' **prop_choro**: displays symbols with sizes proportional to values of a
 #' first variable and colored to reflect the classification of a second
 #' quantitative variable.
 #' \preformatted{
@@ -121,7 +122,7 @@
 #'        leg_box_cex = c(1, 1), add = TRUE)
 #'        }
 #'
-#' * **prop_typo**: displays symbols with sizes proportional to values of a
+#' **prop_typo**: displays symbols with sizes proportional to values of a
 #' first variable and colored to reflect the modalities of a second qualitative
 #' variable.
 #' \preformatted{
@@ -139,7 +140,7 @@
 #'        add = TRUE)
 #'        }
 #'
-#' * **symb_choro**: displays the different modalities of a first qualitative
+#' **symb_choro**: displays the different modalities of a first qualitative
 #' variable as symbols colored to reflect the classification of a second
 #' quantitative variable.
 #' \preformatted{
@@ -233,14 +234,42 @@
 #'
 #'
 #' @examples
+#' library(mapsf)
 #' mtq <- mf_get_mtq()
+#' # basic examples
 #' # type = "base"
 #' mf_map(mtq)
-#' mf_map(mtq, type = "base", col = "lightblue", lwd = 1.5, lty = 2)
-#'
 #' # type = "prop"
 #' mf_map(mtq)
 #' mf_map(mtq, var = "POP", type = "prop")
+#' # type = "choro"
+#' mf_map(mtq, var = "MED", type = "choro")
+#' # type = "typo"
+#' mf_map(mtq, "STATUS", "typo")
+#' # type = "symb"
+#' mf_map(mtq)
+#' mf_map(mtq, "STATUS", "symb")
+#' # type = "grad"
+#' mf_map(mtq)
+#' mf_map(mtq, var = "POP", type = "grad")
+#' # type = "prop_choro"
+#' mf_map(mtq)
+#' mf_map(mtq, var = c("POP", "MED"), type = "prop_choro")
+#' # type = "prop_typo"
+#' mf_map(mtq)
+#' mf_map(mtq, var = c("POP", "STATUS"), type = "prop_typo")
+#' # type = "symb_choro
+#' mf_map(mtq)
+#' mf_map(mtq, var = c("STATUS", "MED"), type = "symb_choro")
+#'
+#'
+#'
+#'
+#' # detailed examples
+#' # type = "base"
+#' mf_map(mtq, type = "base", col = "lightblue", lwd = 1.5, lty = 2)
+#'
+#' # type = "prop"
 #' mf_map(mtq)
 #' mf_map(
 #'   x = mtq, var = "POP", type = "prop",
@@ -252,7 +281,6 @@
 #' )
 #'
 #' # type = "choro"
-#' mf_map(mtq, var = "MED", type = "choro")
 #' mtq[6, "MED"] <- NA
 #' mf_map(
 #'   x = mtq, var = "MED", type = "choro",
@@ -265,8 +293,7 @@
 #' )
 #'
 #' # type = "typo"
-#' mf_map(mtq, "STATUS", "typo")
-#' mtq[6, "STATUS"] <- NA
+#' mtq[4, "STATUS"] <- NA
 #' mf_map(
 #'   x = mtq, var = "STATUS", type = "typo",
 #'   pal = c("red", "blue", "yellow"), lwd = 1.1,
@@ -280,25 +307,67 @@
 #'
 #' # type = "symb"
 #' mf_map(mtq)
-#' mf_map(mtq, "STATUS", "symb")
-#' mf_map(mtq)
 #' mf_map(
 #'   x = mtq, var = "STATUS", type = "symb",
 #'   pch = c(21:23), pal = c("red1", "tan1", "khaki1"),
-#'   border = "grey20", cex = c(1.5, 1, .9), lwd = .5,
+#'   border = "grey20", cex = c(2, 1.5, 1), lwd = .5,
 #'   val_order = c("Prefecture", "Sub-prefecture", "Simple municipality"),
 #'   pch_na = 24, col_na = "blue", leg_frame = TRUE
 #' )
 #'
 #' # type = "grad"
 #' mf_map(mtq)
-#' mf_map(mtq, var = "POP", type = "grad")
+#' mf_map(mtq, var = "POP", type = "grad", pch = 22,
+#'        breaks = "quantile", nbreaks = 4, lwd = 2, border = "blue",
+#'        cex = c(.75,1.5,3,5), col = "lightgreen")
+#'
+#' # type = "prop_choro"
 #' mf_map(mtq)
-#' mf_map(mtq, var = c("POP", "MED"), type = "prop_choro")
+#' mf_map(
+#'   x = mtq, var = c("POP", "MED"), type = "prop_choro",
+#'   inches = .35, border = "tomato4",
+#'   val_max = 90000, symbol = "circle", col_na = "white", pal = "Cividis",
+#'   breaks = "equal", nbreaks = 4, lwd = 4,
+#'   leg_pos =  "bottomleft",
+#'   leg_title = c("Population", "Median Income"),
+#'   leg_title_cex = c(0.8, 1),
+#'   leg_val_cex = c(.7, .9),
+#'   leg_val_rnd = c(0, 0),
+#'   leg_no_data = "No data",
+#'   leg_frame = c(TRUE, TRUE),
+#'   add = TRUE
+#' )
+#'
+#' # type = "prop_typo"
 #' mf_map(mtq)
-#' mf_map(mtq, var = c("POP", "STATUS"), type = "prop_typo")
+#' mf_map(
+#'   x = mtq, var = c("POP", "STATUS"), type = "prop_typo",
+#'   inches = .35, border = "tomato4",
+#'   val_max = 90000, symbol = "circle", col_na = "white", pal = "Dynamic",
+#'   lwd = 2,
+#'   leg_pos = c("bottomright", "bottomleft"),
+#'   leg_title = c("Population", "Municipality\nstatus"),
+#'   leg_title_cex = c(0.9, 0.9),
+#'   leg_val_cex = c(.7, .7),
+#'   val_order = c("Prefecture", "Sub-prefecture", "Simple municipality"),
+#'   leg_no_data = "No dada",
+#'   leg_frame = c(TRUE, TRUE),
+#'   add = TRUE
+#' )
+#'
+#' # type = "symb_choro"
 #' mf_map(mtq)
-#' mf_map(mtq, var = c("STATUS", "MED"), type = "symb_choro")
+#' mf_map(
+#'   mtq, c("STATUS", "MED"), type = "symb_choro",
+#'   pal = "Reds 3", breaks = "quantile", nbreaks = 4,
+#'   pch = 21:23, cex = c(3, 2, 1),
+#'   pch_na = 25, cex_na = 1.5, col_na = "blue",
+#'   val_order = c(
+#'     "Prefecture",
+#'     "Sub-prefecture",
+#'     "Simple municipality"
+#'   )
+#' )
 mf_map <- function(x,
                    var,
                    type = "base",
