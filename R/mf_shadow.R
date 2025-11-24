55c1e5455229bbc3554e27ef9f73a4288cb5994f
rCarto <timothee.giraud@cnrs.fr>
2025-11-24 11:20:47 +0100

    fix: compute proper mf_shadow() offset on empty plots

--- R/mf_shadow.R
+++ R/mf_shadow.R
@@ -15,18 +15,15 @@
 mf_shadow <- function(x, col, cex = 1, add = FALSE) {
   op <- par(mar = getOption("mapsf.mar"), no.readonly = TRUE)
   on.exit(par(op))
-  xyi <- xyinch(1)
-  ratio <- xyi[2] / xyi[1]
-
   if (add == FALSE) {
     mf_init(x)
   }
   col <- go(col, "highlight", "grey50")
+  xyi <- xyinch(1)
+  ratio <- xyi[2] / xyi[1]
   d_x <- cex * strwidth("M", units = "user", cex = 1) / 3
   d_y <- d_x * ratio
-
   z <- st_geometry(x) + c(d_x, -d_y)
-
   mf_map(
     x = z, type = "base", col = col,
     border = col, lwd = .5, add = TRUE
