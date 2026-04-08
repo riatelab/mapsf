86808984e1cff9322de54386d115831b3b60b39a
rCarto <timothee.giraud@cnrs.fr>
2026-04-08 16:43:10 +0200

    tests: fix text

--- inst/tinytest/test_get_ratio.R
+++ inst/tinytest/test_get_ratio.R
@@ -1,7 +1,7 @@
 expect_silent(mf_get_ratio(mtq, height = 6))
 expect_silent(mf_get_ratio(mtq, width = 7))
 expect_silent(mf_get_ratio(mtq))
-expect_equal(mf_get_ratio(mtq, theme = "default"), c(7, 8.403))
+expect_equal(mf_get_ratio(mtq, theme = "base"), c(7, 8.513))
 expect_silent(mf_get_ratio(mtq, width = 3, expandBB = c(0, 0, 0, .3),
                            theme = "sol_dark"))
 expect_silent(mf_get_ratio(sf::st_transform(mtq, 4326)))
