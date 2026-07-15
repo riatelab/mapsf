e1c5886816bc12364e0303ccfe48948c2db3cf15
rCarto <timothee.giraud@cnrs.fr>
2026-07-15 15:41:32 +0200

    test: update mf_logo tests

--- inst/tinytest/test_logo.R
+++ inst/tinytest/test_logo.R
@@ -1,18 +1,18 @@
 mf_map(mtq)
 expect_silent(mf_logo(system.file("img/background.jpg",
-                                        package = "mapsf")))
+                                  package = "mapsf")))
 expect_silent(mf_logo(system.file("help/figures/logo.png",
-                                        package = "mapsf"),
+                                  package = "mapsf"),
                       pos = "topright",
                       adj = c(-10,-10), cex = 1))
 expect_silent(mf_logo(system.file("help/figures/logo.png",
                                   package = "mapsf"),
                       pos = c(690000, 1625355.65627547),
                       cex = 2.5))
-expect_silent(mf_logo(system.file("help/figures/logo.png",
+expect_silent(mf_logo(system.file("img/background.jpg",
                                   package = "mapsf"),
                       pos = c(690000, 1625355.65627547),
                       resize = FALSE))
-expect_error(mf_logo(system.file("help/figures/logo.png",
-                                  package = "mapsf"),
-                      pos = "toplaft"))
+expect_error(mapsf:::posinset("topleeft", pusr = graphics::par("usr"),
+                              wdest = 500, hdest = 500, adj = c(0,0)))
+
